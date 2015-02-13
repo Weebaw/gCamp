4bb288146a78bd0394a98fbd01cac4a3ffdfcf50
Ben Steinbook <ben.steinbook@gmail.com>
2015-02-13 15:22:42 -0700

    date adn other fixes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -41,6 +41,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:description, :complete)
+    params.require(:task).permit(:description, :complete, :due_date)
   end
 end
