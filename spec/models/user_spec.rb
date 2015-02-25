127ec7e2a19058fae96ddc36323b82af07a7f81b
Ben Steinbook <ben.steinbook@gmail.com>
2015-02-25 15:16:33 -0700

    travis might actually work now

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
   describe User do
     it "requires a first name" do
-      User.create( first_name: "Ben", last_name: "Steinbook", email: "ben@ben.com" )
+      user = User.create( first_name: "Ben", last_name: "Steinbook", email: "ben@ben.com" )
       expect(user).to be_valid
     end
   end
